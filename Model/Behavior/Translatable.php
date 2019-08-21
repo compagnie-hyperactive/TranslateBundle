494035235e92b0b9bc7b958fdffb6a769f747745
Nicolas BONNIOT <nicolas@devgiants.fr>
2019-08-21 14:10:50 +0200

    Add serialisation group for translate field
    Also add symfony/serializer dependency.

--- Model/Behavior/Translatable.php
+++ Model/Behavior/Translatable.php
@@ -5,6 +5,7 @@ namespace Lch\TranslateBundle\Model\Behavior;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * Trait Translatable
@@ -14,6 +15,7 @@ trait Translatable
 {
     /**
      * @ORM\Column(type="string", length=15, nullable=false)
+     * @Groups({"lch_translate"})
      */
     protected $language;
 
